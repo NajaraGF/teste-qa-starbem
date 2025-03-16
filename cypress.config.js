be5ce6cbc345f9f3c9ca03f45ea644f504238408
NajaraGF <najaguiland@gmail.com>
2025-03-16 17:08:25 -0300

    Remove configuração desnecessária do bundler e do plugin Cucumber no Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,11 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
-      // Configura o bundler para interpretar arquivos .feature
-      const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      });
-      on("file:preprocessor", bundler);
-
-      // Adiciona o plugin do Cucumber
-      await addCucumberPreprocessorPlugin(on, config);
-      
+    baseUrl: "https://bugbank.netlify.app/",  // URL base
+    setupNodeEvents(on, config) {
+     
       return config;
-    },
-    specPattern: "cypress/e2e/features/*.feature",  // Pasta dos arquivos .feature
-    supportFile: "cypress/support/e2e.js",           // Arquivo de suporte global
-    baseUrl: "https://bugbank.netlify.app/",          // URL base da aplicação
-  },
+    }
+  }
 });
